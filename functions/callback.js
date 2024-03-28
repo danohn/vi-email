6146f2aa17c70b234cae2fe7b438087c8fb544e5
danohn <82357071+danohn@users.noreply.github.com>
2024-03-28 12:28:23 +1100

    Main commit

--- functions/callback.js
+++ functions/callback.js
@@ -1,5 +1,55 @@
-exports.handler = function(context, event, callback) {
-  const twiml = new Twilio.twiml.VoiceResponse();
-  twiml.say('Hello World!');
-  callback(null, twiml);
+const swapRoles = true;
+const sgMail = require("@sendgrid/mail");
+
+exports.handler = async (context, event, callback) => {
+  sgMail.setApiKey(context.SENDGRID_API_SECRET);
+
+  const transcriptSid = event.transcript_sid;
+  const client = context.getTwilioClient();
+
+  try {
+    let transcript = await client.intelligence.v2
+      .transcripts(transcriptSid)
+      .fetch();
+
+    let participants = transcript.channel.participants;
+
+    sentences = await client.intelligence.v2
+      .transcripts(transcriptSid)
+      .sentences.list();
+
+    let participantMap = participants.reduce((acc, participant) => {
+      if (swapRoles) {
+        acc[participant.channel_participant === 1 ? 2 : 1] = participant.role;
+      } else {
+        acc[participant.channel_participant] = participant.role;
+      }
+      return acc;
+    }, {});
+
+    let concatenatedTranscripts = "";
+
+    for (let sentence of sentences) {
+      const role = participantMap[sentence.mediaChannel];
+      concatenatedTranscripts += `<p><strong>${role}:</strong> ${sentence.transcript}</p>\n`;
+    }
+
+    const msg = {
+      to: context.TO_EMAIL_ADDRESS,
+      from: {
+        name: "Voice Intelligence Transcripts",
+        email: context.FROM_EMAIL_ADDRESS,
+      },
+      html: `${concatenatedTranscripts}`,
+      subject: "New Voice Intelligence Transcript",
+    };
+
+    message = await sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+    return callback(error);
+  }
+
+  let resp = "";
+  return callback(null, resp);
 };
